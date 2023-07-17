3f8d37d22289c7b75c6a3cab626a4060061f0bce
Alan Ruanova <ruanovaalan96@gmail.com>
2023-07-17 12:00:18 -0600

    Add boilerplate, navbar and index ejs files

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const ejsMate = require("ejs-mate");
 const mongoose = require('mongoose');
 
+//Connection to mongoose
+mongoose.connect("mongodb://127.0.0.1:27017/Recipes");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection erro:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//Set the app
+const app = express();
+
+//Set engine view and path
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//Set the url encoded to parse the body
+app.use(express.urlencoded({ extended: true }));
+
+
+//Routes
 app.get('/recipes', (req, res) => {
-    res.send('HELLO')
+    res.render('recipes/index');
 })
 
 app.listen(3000, () => {
