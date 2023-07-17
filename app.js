29a355c4aa0bb440ae1542f2a467c65c1ef2de4c
Alan Ruanova <ruanovaalan96@gmail.com>
2023-07-17 11:44:30 -0600

    Add views, layouts, partials and recipes folders

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+app.get('/recipes', (req, res) => {
+    res.send('HELLO')
+})
+
 app.listen(3000, () => {
     console.log("Serving Port 3000");
 })
