e21d55460ae62463d505b2a3b60530000dbde524
Alan Ruanova <ruanovaalan96@gmail.com>
2023-08-03 17:41:23 -0600

    Add review form to show page

--- models/favorite.js
+++ models/favorite.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Recipe = require('./recipe');
-const User = require('./user');
 
 const favoriteSchema = new Schema({
     status: {
