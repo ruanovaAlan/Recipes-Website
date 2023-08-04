3b8c11ae0b7691fdc27171f7ff24d1f5b5b0cf3a
Alan Ruanova <ruanovaalan96@gmail.com>
2023-08-03 22:45:38 -0600

    Refactor reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,25 +8,20 @@ router.post('/', storeReturnTo, async (req,res) => {
     const recipe = await Recipe.findById(req.body.id);
     const review = new Review(req.body.review);
     review.author = req.user._id;
-    console.log(recipe, review)
     recipe.reviews.push(review);
     await review.save();
     await recipe.save();
-    console.log(recipe, review)
     req.flash('success', 'Review creado correctamente');
     res.redirect(`/recipes/${recipe.id}`);
 })
 
 router.delete('/:reviewId', async(req,res) => {
     const { id, reviewId } = req.params;
-    console.log(id, reviewId)
     await Recipe.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     //$pull removes a specified document from an array
     await Review.findById(reviewId);
     req.flash('success', 'Review deleted successfully!');
     res.redirect(`/recipes/${id}`);
 })
-
-
 
 module.exports = router;
