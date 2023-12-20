7e449290503c027367e5fbbc43ae3c44b2946478
Alan Ruanova <ruanovaalan96@gmail.com>
2023-12-20 12:03:16 -0600

    Fix seed.js insertion into mongo

--- Data/seed.js
+++ Data/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Recipe = require('../models/recipes');
+const Recipe = require('../models/recipe');
 
 mongoose.connect('mongodb://127.0.0.1:27017/Recipes')
     .then(() => {
